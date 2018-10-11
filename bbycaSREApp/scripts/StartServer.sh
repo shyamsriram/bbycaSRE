5033fefb293af6ce5a2d05ed212dbd54facc3c73
Shyam Sriram <shyamps@gmail.com>
2018-10-11 20:58:06 +0000

    Added Dev group Execution Logic

--- bbycaSREApp/scripts/StartServer.sh
+++ bbycaSREApp/scripts/StartServer.sh
@@ -1,23 +1,40 @@
 #!/bin/sh
 
 STAGING="/var/bbycasre/staging"
+LOG="/var/log/bbycaSRE.log"
 DEPLOYPATH="/opt/bbycaSRE/DEV"
 ENV="DEV"
 PORT="8000"
-LOG="/var/log/bbycaSRE.log"
+
+#Define Environment Variables for 
+DEVENV="DEV"
+DEVPORT="8001"
+TESTENV="TEST"
+TESTPORT="8002"
+DRENV="DR"
+DRPORT="8003"
+PRODENV="PROD"
+PRODPORT="8004"
 
 source ~/.bashrc
 
-date +"%d-%b-%Y %H:%M:%S (%Z) ENV: $ENV PORT: $PORT CURDIR:$CURDIR DEPLOYPATH: $DEPLOYPATH DEPLOYMENT GRP: $DEPLOYMENT_GROUP_NAME" >> /var/log/bbycaSRE.log
+date +"%d-%b-%Y %H:%M:%S (%Z) Executing Configuration ENV: $ENV PORT: $PORT DEPLOYPATH: $DEPLOYPATH DEPLOYMENT GRP: $DEPLOYMENT_GROUP_NAME" >> /var/log/bbycaSRE.log
 
-export ENV=$ENV
-export PORT=$PORT
+# Set Env Variables Copy Deployment folder and Start Server
+if [ "$DEPLOYMENT_GROUP_NAME" = "Development" ]
+then
 
-mkdir -p $DEPLOYPATH
+	export ENV=$DEVENV
+	export PORT=$DEVPORT
+	mkdir -p $DEPLOYPATH/$DEVENV
 
-cp -prf $STAGING/* $DEPLOYPATH
+	cp -prf $STAGING/* $DEPLOYPATH/$DEVENV
 
-date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy Node Js Application...!!!" >> $LOG
+	date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy Node Js Application...!!!" >> $LOG
 
-#Start the Server in background
-node $DEPLOYPATH/bbycaSREApp/src/bestbuy.ca.js 1>>$LOG 2>>$LOG &
+	#Start the Server in background
+	node $DEPLOYPATH/bbycaSREApp/src/bestbuy.ca.js 1>>$LOG 2>>$LOG &
+else
+	#Fail and Exit
+	exit 1
+fi
