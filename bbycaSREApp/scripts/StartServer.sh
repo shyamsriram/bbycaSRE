2eaef7447931e67a8fe597cc8b756d614148dd1a
Shyam Sriram <shyamps@gmail.com>
2018-10-11 19:08:39 +0000

    Fixed Start Server Script Issues

--- bbycaSREApp/scripts/StartServer.sh
+++ bbycaSREApp/scripts/StartServer.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-CURDIR=`pwd`
+STAGING="/var/bbycasre/staging/"
 DEPLOYPATH="/opt/bbycaSREApp/DEV"
 ENV="DEV"
 PORT="8000"
 LOG="/var/log/bbycaSRE.log/var/log/bbycaSRE.log"
 
-date +"%d-%b-%Y %H:%M:%S (%Z) ENV: $ENV PORT: $PORT CURDIR:$CURDIR DEPLOYPATH: $DEPLOYPATH" >> /var/log/bbycaSRE.log
+date +"%d-%b-%Y %H:%M:%S (%Z) ENV: $ENV PORT: $PORT CURDIR:$CURDIR DEPLOYPATH: $DEPLOYPATH DEPLOYMENT GRP: $DEPLOYMENT_GROUP_NAME" >> /var/log/bbycaSRE.log
 
 export ENV=$ENV
 export PORT=$PORT
 
 mkdir -p $DEPLOYPATH
 
-cp -prf $CURDIR/../* $DEPLOYPATH
+cp -prf $STAGING/../* $DEPLOYPATH
 
 date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy Node Js Application...!!!" >> /var/log/bbycaSRE.log
 
