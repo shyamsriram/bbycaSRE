7b58a04a6c153eadd6abd20bd1fd20491889abea
Shyam Sriram <shyamps@gmail.com>
2018-10-11 19:29:16 +0000

    Fixed Start Server Script install Path

--- bbycaSREApp/scripts/StartServer.sh
+++ bbycaSREApp/scripts/StartServer.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-STAGING="/var/bbycasre/staging/"
-DEPLOYPATH="/opt/bbycaSREApp/DEV"
+STAGING="/var/bbycasre/staging"
+DEPLOYPATH="/opt/bbycaSRE/DEV"
 ENV="DEV"
 PORT="8000"
-LOG="/var/log/bbycaSRE.log/var/log/bbycaSRE.log"
+LOG="/var/log/bbycaSRE.log"
 
 date +"%d-%b-%Y %H:%M:%S (%Z) ENV: $ENV PORT: $PORT CURDIR:$CURDIR DEPLOYPATH: $DEPLOYPATH DEPLOYMENT GRP: $DEPLOYMENT_GROUP_NAME" >> /var/log/bbycaSRE.log
 
@@ -13,9 +13,9 @@ export PORT=$PORT
 
 mkdir -p $DEPLOYPATH
 
-cp -prf $STAGING/../* $DEPLOYPATH
+cp -prf $STAGING/* $DEPLOYPATH
 
-date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy Node Js Application...!!!" >> /var/log/bbycaSRE.log
+date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy Node Js Application...!!!" >> $LOG
 
 #Start the Server in background
-node $DEPLOYPATH/src/bestbuy.ca.js &
+node $DEPLOYPATH/bbycaSREApp/src/bestbuy.ca.js 1>>$LOG 2>>$LOG &
