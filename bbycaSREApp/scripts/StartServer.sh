b399f2e375382b063ea14286daeceb9372cb4612
Shyam Sriram <shyamps@gmail.com>
2018-10-11 21:25:33 +0000

    Added Execution Logic for Deployment Groups Test DR Prod

--- bbycaSREApp/scripts/StartServer.sh
+++ bbycaSREApp/scripts/StartServer.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+#Initialize Paths
 STAGING="/var/bbycasre/staging"
 LOG="/var/log/bbycaSRE.log"
-DEPLOYPATH="/opt/bbycaSRE/DEV"
-ENV="DEV"
-PORT="8000"
+DEPLOYPATH="/opt/bbycaSRE/"
 
-#Define Environment Variables for 
+#Define Environment Variables for DEV, TEST, DR and PROD 
 DEVENV="DEV"
 DEVPORT="8001"
 TESTENV="TEST"
@@ -23,17 +22,44 @@ date +"%d-%b-%Y %H:%M:%S (%Z) Executing Configuration ENV: $ENV PORT: $PORT DEPL
 # Set Env Variables Copy Deployment folder and Start Server
 if [ "$DEPLOYMENT_GROUP_NAME" = "Development" ]
 then
-
 	export ENV=$DEVENV
 	export PORT=$DEVPORT
 	mkdir -p $DEPLOYPATH/$DEVENV
-
 	cp -prf $STAGING/* $DEPLOYPATH/$DEVENV
-
-	date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy Node Js Application...!!!" >> $LOG
-
+	date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy $ENV Node Js Application...!!!" >> $LOG
 	#Start the Server in background
 	node $DEPLOYPATH/bbycaSREApp/src/bestbuy.ca.js 1>>$LOG 2>>$LOG &
+
+elif [ "$DEPLOYMENT_GROUP_NAME" = "Test" ]
+then
+	export ENV=$TESTENV
+	export PORT=$TESTPORT
+	mkdir -p $DEPLOYPATH/$TESTENV
+	cp -prf $STAGING/* $DEPLOYPATH/$TESTENV
+	date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy $ENV Node Js Application...!!!" >> $LOG
+	#Start the Server in background
+	node $DEPLOYPATH/bbycaSREApp/src/bestbuy.ca.js 1>>$LOG 2>>$LOG &
+
+elif [ "$DEPLOYMENT_GROUP_NAME" = "Disaster" ]
+then
+	export ENV=$DRENV
+	export PORT=$DRPORT
+	mkdir -p $DEPLOYPATH/$DRENV
+	cp -prf $STAGING/* $DEPLOYPATH/$DRENV
+	date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy $ENV Node Js Application...!!!" >> $LOG
+	#Start the Server in background
+	node $DEPLOYPATH/bbycaSREApp/src/bestbuy.ca.js 1>>$LOG 2>>$LOG &
+
+elif [ "$DEPLOYMENT_GROUP_NAME" = "Production" ]
+then
+	export ENV=$PRODENV
+	export PORT=$PRODPORT
+	mkdir -p $DEPLOYPATH/$PRODENV
+	cp -prf $STAGING/* $DEPLOYPATH/$PRODENV
+	date +"%d-%b-%Y %H:%M:%S (%Z) Starting Best Buy $ENV Node Js Application...!!!" >> $LOG
+	#Start the Server in background
+	node $DEPLOYPATH/bbycaSREApp/src/bestbuy.ca.js 1>>$LOG 2>>$LOG &
+
 else
 	date +"%d-%b-%Y %H:%M:%S (%Z) Unknown Deployment Group: $DEPLOYMENT_GROUP_NAME Detected!!! Deployment has Failed!!!" >> $LOG
 	#Fail and Exit
