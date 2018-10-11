2ede1e72b075b97a93705c84c75cecfc98268c62
Shyam Sriram <shyamps@gmail.com>
2018-10-11 23:35:02 +0000

    Add Deployment Group Logging

--- bbycaSREApp/scripts/before.sh
+++ bbycaSREApp/scripts/before.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-date +"%d-%b-%Y %H:%M:%S (%Z) AWS Deployment Started...." > /var/log/bbycaSRE.log
+date +"%d-%b-%Y %H:%M:%S (%Z) AWS $DEPLOYMENT_GROUP_NAME Deployment Started...." > /var/log/bbycaSRE.log
 
 #Clean Up any Previous Deployments
 rm -rf /var/bbycasre 
